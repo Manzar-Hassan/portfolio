d9ac11987aa485c8a392d49becbbe41748cabdde
Manzar Hassan <manzarhassan05@gmail.com>
2022-09-16 20:25:38 +0530

    made directory for project info

---
+++ src/data/Data.js
@@ -0,0 +1,46 @@
+export const aboutProject = [
+  "An Application for billing customer products",
+  "An Application to prdict the country of anyone's name",
+  "An Application for student's complain,food and lost & found items",
+  "An Application for booking movie tickets online",
+];
+
+export const projectNames = [
+  "Inventory Billing Application",
+  "Find me",
+  "All-in-one Application",
+  "Book your seat",
+];
+
+export const projectNamesLinks = [
+  "https://inventory-billing-05.netlify.app",
+  "https://find-me-05-v2.netlify.app",
+  "https://all-in-one-05.netlify.app",
+  "https://live-tv-05.netlify.app",
+];
+
+export const otherProjects = [
+  "Library management app",
+  "Student admin app",
+  "Users crud app",
+  "Search movie app",
+  "random quotes app",
+  "country and weather app",
+  "calculator",
+  "pagination",
+  "discord login page",
+  "loading screen",
+];
+
+export const otherProjectsLinks = [
+  "https://library-05.netlify.app/",
+  "https://student-teacher-05.netlify.app/",
+  "https://users-05.netlify.app/",
+  "https://my-movies-05.netlify.app/",
+  "https://quotes-05.netlify.app/",
+  "https://country-and-weather.netlify.app/",
+  "https://calculator-05.netlify.app/",
+  "https://pagination-05.netlify.app/",
+  "https://discord-login-1.netlify.app/",
+  "https://loading-screen-1.netlify.app/",
+];
--- src/pages/projects/Projects.js
+++ src/pages/projects/Projects.js
@@ -7,6 +7,13 @@ import { GiTicket } from "react-icons/gi";
 import { SiCodeproject } from "react-icons/si";
 import { Stack } from "@mui/system";
 import {
+  aboutProject,
+  projectNames,
+  projectNamesLinks,
+  otherProjects,
+  otherProjectsLinks,
+} from "../../data/Data";
+import {
   StyledBlueEffect,
   StyledButton,
   StyledSkillCard,
@@ -18,53 +25,6 @@ const projects = [
   <FaSearchLocation style={{ fontSize: "10rem", color: "#f99f38" }} />,
   <FaUniversity style={{ fontSize: "10rem", color: "#f99f38" }} />,
   <GiTicket style={{ fontSize: "10rem", color: "#f99f38" }} />,
-];
-
-const aboutProject = [
-  "An Application for billing customer products",
-  "An Application to prdict the country of anyone's name",
-  "An Application for student's complain,food and lost & found items",
-  "An Application for booking movie tickets online",
-];
-
-const projectNames = [
-  "Inventory Billing Application",
-  "Find me",
-  "All-in-one Application",
-  "Book your seat",
-];
-
-const projectNamesLinks = [
-  "https://inventory-billing-05.netlify.app",
-  "https://find-me-05-v2.netlify.app",
-  "https://all-in-one-05.netlify.app",
-  "https://live-tv-05.netlify.app",
-];
-
-const otherProjects = [
-  "Library management app",
-  "Student admin app",
-  "Users crud app",
-  "Search movie app",
-  "random quotes app",
-  "country and weather app",
-  "calculator",
-  "pagination",
-  "discord login page",
-  "loading screen",
-];
-
-const otherProjectsLinks = [
-  "https://library-05.netlify.app/",
-  "https://student-teacher-05.netlify.app/",
-  "https://users-05.netlify.app/",
-  "https://my-movies-05.netlify.app/",
-  "https://quotes-05.netlify.app/",
-  "https://country-and-weather.netlify.app/",
-  "https://calculator-05.netlify.app/",
-  "https://pagination-05.netlify.app/",
-  "https://discord-login-1.netlify.app/",
-  "https://loading-screen-1.netlify.app/",
 ];
 
 const Projects = () => {
